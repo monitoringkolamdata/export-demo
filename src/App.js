582bbea66b6a19c9505cc483f282b2e21b8f1690
monitoringkolam <azisiska@gmail.com>
2018-08-14 12:26:17 +0700

    Step 2: Rough version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import React, { Component } from 'react'
+class App extends Component {   
+  render() {     
+    return (       
+      <div style={style}>
+         <div>
+           <h1>Export Demo</h1>
+           <button>Export to Excel</button>
+           <table>
+             <tr>
+               <th>Firstname</th>
+               <th>Lastname</th>
+               <th>Age</th>
+             </tr>
+             <tr>
+               <td>Elon</td>
+               <td>Musk</td>
+               <td>23</td>
+             </tr>
+             <tr>
+               <td>Donald</td>
+               <td>Trump</td>
+               <td>94</td>
+             </tr>
+           </table>
+         </div>
+       </div>
+     )
+   }
+ }
+const style = {
+  display: 'flex',
+  justifyContent: 'center'
 }
-
-export default App;
+export default App
